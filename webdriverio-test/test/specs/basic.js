5d88be55a99dd6f1266cddbbd5e32d36e7ff7168
Palani <palani.mv@gmail.com>
2020-08-27 02:32:00 +0530

    Initial WebDriverio

---
+++ webdriverio-test/test/specs/basic.js
@@ -0,0 +1,10 @@
+describe('webdriver.io page', () => {
+    it('should have the right title', () => {
+        browser.url('https://webdriver.io')
+        const elem = $('.projectLogo').$$( "img" )[0]
+        console.log("The Image is" + elem.getAttribute("src"))
+        expect(elem).
+        expect(elem).toHaveAttribute('src','https://webdriver.io/img/webdriverio.png');
+
+    })
+})
